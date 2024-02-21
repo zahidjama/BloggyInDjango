36c8c7eed4800b9d5aa9aa94d2c8e81fa95fd730
zahidjama <zahid.jamali4848@gmail.com>
2024-02-22 01:29:38 +0500

    Just setup the working enviroment with templates and static

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def loginFunc(request):
+    return render(request, "login.html")
+
+def signinFunc(request):
+    return render(request, "signin.html")
